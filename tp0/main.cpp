1eddb059ffe79851d87fe8d7e0ffa73bac5e2e9c
François Bélanger <belf1801@usherbrooke.ca>
2017-05-15 16:53:34 -0400

    initial commit

---
+++ tp0/main.cpp
@@ -0,0 +1,36 @@
+/*
+	Francois Belanger 94 245 437
+*/
+
+//TODO: fbr to remove
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+}
+
+/*
+
+#include <regex>
+
+int main()
+{
+
+}
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
+*/
