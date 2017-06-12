d4773e4cb6a7d9d718e4b3daa2354760f7a1f414
Francois Belanger <francois.belanger3@usherbroke.ca>
2017-06-12 19:54:08 -0400

    some more progress

--- tp0/main.cpp
+++ tp0/main.cpp
@@ -13,33 +13,44 @@
 
 #include <fstream>
 #include <sstream>
+#include <chrono>
+//TODO faire le menage
+#include <algorithm>
+#include <set>
 
 using namespace std;
+using namespace chrono;
 
-#define KEYWORDS string keywords [] = { "alignas", "alignof", "and", "and_eq", "asm", "atomic_cancel", "atomic_commit", "atomic_noexcept", "auto", "bitand", "bitor", "bool", "break", "case",\
+#define KEYWORDS set<string> keywords = { "alignas", "alignof", "and", "and_eq", "asm", "atomic_cancel", "atomic_commit", "atomic_noexcept", "auto", "bitand", "bitor", "bool", "break", "case",\
 "catch", "char", "char16_t", "char32_t", "class", "compl", "concept", "const", "constexpr", "const_cast", "continue", "decltype", "default", "delete", "do", "double", "dynamic_cast", "else",\
 "enum", "explicit", "export", "extern", "false", "float", "for", "friend", "goto", "if", "inline", "int", "import", "long", "module", "mutable", "namespace", "new", "noexcept", "not", "not_eq",\
 "nullptr", "operator", "or", "or_eq", "private", "protected", "public", "register", "reinterpret_cast", "requires", "return", "short", "unsigned", "signed", "sizeof", "static", "static_assert",\
 "static_cast", "struct", "switch", "synchronized", "template", "this", "thread_local", "throw", "true", "try", "typedef", "typeid", "typename", "union", "using", "virtual", "void", "volatile",\
 "wchar_t", "while", "xor", "xor_eq" };
 
+
 //TODO: passer au memory map
+//milliseconds totalT0 = milliseconds{ 0 };
 string t0(const string fname)
 {
+	//auto before = chrono::high_resolution_clock::now();
 	ifstream file{ fname };
 	stringstream ss;
 	string line;
-	string backslach_n("\n");
+	string backslach_n{ '\n' };
 
 	//TODO: fbr utiliser des iterateurs de stream
 	while (getline(file, line))
 		ss << line << backslach_n;
 
+	//totalT0 += duration_cast<milliseconds>(high_resolution_clock::now() - before);
 	return ss.str();
 }
 
+//milliseconds totalT1 = milliseconds{ 0 };
 string t1(string& toClean)
 {
+	//auto before = chrono::high_resolution_clock::now();
 	//TODO: a remanier.
 	for (string::size_type i = 0; (i = toClean.find("&", i)) != string::npos;)
 	{
@@ -56,41 +67,53 @@ string t1(string& toClean)
 		toClean.replace(i, string{ ">" }.length(), "&gt;");
 		i += string{ "&gt;" }.length();
 	}
+
+	//totalT1 += duration_cast<milliseconds>(high_resolution_clock::now() - before);
 	return toClean;
 }
 
-//TODO a revoir
-//string t2(const vector<string>& cppKeywords, string& inFile)
-string t2(string& inFile)
+//milliseconds totalT2 = milliseconds{ 0 };
+string t2(string& inLine)
 {
-	string spanOpen = "<span style = 'color:blue'>";
+	//auto before = chrono::high_resolution_clock::now();
+	string spanOpen = "<span id=\"t2\">";
 	string spanClose = "</span>";
+	string word;
 	KEYWORDS;
 
-	for (const string& keyWord : keywords)
+	istringstream i{ inLine };
+	ostringstream o;
+	bool tag = false;
+
+	auto lastIdx = i.tellg();
+	while (i >> word)
 	{
-		for (string::size_type i = 0; (i = inFile.find(keyWord, i)) != string::npos;)
+		//cout << i.tellg() - lastIdx << " " << (i.tellg() - lastIdx) - word.length() << endl;
+		//ajouter les char manquant
+		o << inLine.substr(lastIdx, (i.tellg() - lastIdx) - word.length());
+		if (keywords.find(word) != end(keywords))
 		{
-			if (inFile.substr(i, keyWord.length() + 1) == keyWord + " ")
-			{
-				inFile.insert(i, spanOpen);
-				i += (keyWord + spanOpen).length();
-				inFile.insert(i, spanClose);
-				i += 7;
-			}
-			else
-			{
-				i++;
-			}
+			o << spanOpen << word << spanClose;
 		}
+		else
+		{
+			o << word;
+		}
+		lastIdx = i.tellg();
 	}
-	return inFile;
+	o << '\n';
+
+	//totalT2 += duration_cast<milliseconds>(high_resolution_clock::now() - before);
+	return o.str();
 }
 
 //TODO: a refaire
+//milliseconds totalT3 = milliseconds{ 0 };
 string t3(string& inFile)
 {
-	string spanOpen = "<span style = 'color:green'>";
+	//auto before = chrono::high_resolution_clock::now();
+
+	string spanOpen = "<span id=\"t3\">";
 	string spanClose = "</span>";
 	for (string::size_type i = 0; (i = inFile.find("//", i)) != string::npos;)
 	{
@@ -98,7 +121,7 @@ string t3(string& inFile)
 		i += spanOpen.length();
 		auto end = inFile.find("\n", i);
 		i = end;
-		inFile.insert(end + 2, spanClose);
+		inFile.insert(end + 1, spanClose);
 		i += spanClose.length();
 	}
 	for (string::size_type i = 0; (i = inFile.find("/*", i)) != string::npos;)
@@ -110,12 +133,18 @@ string t3(string& inFile)
 		inFile.insert(end + 2, spanClose);
 		i += spanClose.length();
 	}
+
+
+	//totalT3 += duration_cast<milliseconds>(high_resolution_clock::now() - before);
 	return inFile;
 }
 
 //TODO: a refaire
+//milliseconds totalT4 = milliseconds{ 0 };
 string t4(string& inFile)
 {
+	auto before = chrono::high_resolution_clock::now();
+
 	string spanOpen = "<span style = 'color:purple'>";
 	string spanClose = "</span>";
 	for (string::size_type i = 0; (i = inFile.find("#", i)) != string::npos;)
@@ -127,12 +156,16 @@ string t4(string& inFile)
 		inFile.insert(end, spanClose);
 		i += spanClose.length();
 	}
+
+	//totalT4 += duration_cast<milliseconds>(high_resolution_clock::now() - before);
 	return inFile;
 }
 
 //TODO: a templater sur le type de process?
+//milliseconds totalT5 = milliseconds{ 0 };
 void t5(const string& fname, const string& data)
 {
+	//auto before = chrono::high_resolution_clock::now();
 	//TODO: ajouter le type d'execution en parametre
 	ofstream file{ fname + ".html" };
 
@@ -140,26 +173,21 @@ void t5(const string& fname, const string& data)
 	file << "<!DOCTYPE html>\n<html>\n<head>\n";
 	file << "<title>" << fname << "</title>\n";
 	//TODO: add css
-	file << "</title>\n<link rel=\"stylesheet\" type=\"text/css\" href=\"tp0.css\">\n";
+	file << "<link rel=\"stylesheet\" type=\"text/css\" href=\"tp0.css\">\n";
 	file << "</head>\n<body>\n<pre>";
 	//add decorated lines
 	file << data;
 
 	//TODO: add end tag
 	file << "</pre>\n</body>\n</html>";
+
+	//totalT5 += duration_cast<milliseconds>(high_resolution_clock::now() - before);
 }
 
 int main(int argc, const char * argv[])
 {
+	auto before = chrono::high_resolution_clock::now();
 	string fname = "main.cpp";
-
-	string a{ "ceci est in test,	main.cpp a" };
-	istringstream i(a);
-	ostringstream o;
-
-	while (i >> a)
-		cout << a << " " << i.tellg() << endl;
-
 
 	if (argc >= 2)
 	{
@@ -171,10 +199,23 @@ int main(int argc, const char * argv[])
 		//TODO: initialiser un vecteur de nom de fichier
 	}
 
-	for (int i = 0; i < 1000; ++i)
-		t5(fname, t4(t3(t2(t1(t0(fname))))));
+	for (int j = 0; j < 10; ++j)
+	{
+		auto before = chrono::high_resolution_clock::now();
 
-	cout << "asdf";
+		for (int i = 0; i < 1000; ++i)
+			t5(fname, t4(t3(t2(t1(t0(fname))))));
+
+		cout << "total: " << duration_cast<milliseconds>(high_resolution_clock::now() - before).count() << endl;
+	}
+	//cout << "t0 " << totalT0.count() << endl;
+	//cout << "t1 " << totalT1.count() << endl;
+	//cout << "t2 " << totalT2.count() << endl;
+	//cout << "t3 " << totalT3.count() << endl;
+	//cout << "t4 " << totalT4.count() << endl;
+	//cout << "t5 " << totalT5.count() << endl;
+	char a;
+	cin >> a;
 }
 
 /*
